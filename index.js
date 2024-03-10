23a4ed92996bc1584fbfb467243e79f266ad840a
Aryan Kumar <aryankumar877@gmail.com>
2024-03-11 01:18:31 +0530

    upgrade to include stop start and channel input

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
 const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
-const TWITCH_CHANNEL = process.env.TWITCH_CHANNEL;
 
 let spotifyAccessToken = "";
 let spotifyRefreshToken = "";
 let twitchAccessToken = "";
 let twitchRefreshToken = "";
 let lastSong = "";
+let isBotRunning = false;
+let botInterval = null;
+let twitchClient = null;
 
 // Load tokens from file
 try {
@@ -97,107 +99,130 @@ app.get("/callback-twitch", async (req, res) => {
   res.redirect("/");
 });
 
-app.get("/run-bot", async (req, res) => {
-  if (!spotifyAccessToken || !twitchAccessToken) {
-    return res.status(400).send("Both Spotify and Twitch must be authorized.");
+app.get("/toggle-bot", async (req, res) => {
+  const twitchChannel = req.query.channel;
+  if (!twitchChannel) {
+    return res.status(400).json({ message: "Twitch channel is required." });
   }
 
-  const twitchClient = new tmi.Client({
-    options: { debug: false },
-    connection: {
-      reconnect: true,
-    },
-    identity: {
-      username: TWITCH_CHANNEL,
-      password: `oauth:${twitchAccessToken}`,
-    },
-    channels: [TWITCH_CHANNEL],
-  });
+  if (!spotifyAccessToken || !twitchAccessToken) {
+    return res
+      .status(400)
+      .json({ message: "Both Spotify and Twitch must be authorized." });
+  }
 
-  await twitchClient.connect();
+  if (!isBotRunning) {
+    twitchClient = new tmi.Client({
+      options: { debug: false },
+      connection: {
+        reconnect: true,
+      },
+      identity: {
+        username: twitchChannel,
+        password: `oauth:${twitchAccessToken}`,
+      },
+      channels: [twitchChannel],
+    });
+    await twitchClient.connect();
 
-  setInterval(async () => {
-    try {
-      console.log("Checking Spotify for currently playing song...");
-      const response = await axios.get(
-        "https://api.spotify.com/v1/me/player/currently-playing",
-        {
-          headers: {
-            Authorization: `Bearer ${spotifyAccessToken}`,
-          },
-        }
-      );
-      console.log("Spotify response:", response.status, response.data);
-
-      if (response.status === 200) {
-        const data = response.data;
-        const song = `${data.item.name} by ${data.item.artists
-          .map((artist) => artist.name)
-          .join(", ")}`;
-        if (song !== lastSong) {
-          console.log(`Sending message to Twitch chat: Now playing: ${song}`);
-          await twitchClient.say(TWITCH_CHANNEL, `Now playing: ${song}`);
-          lastSong = song;
-        }
-      }
-    } catch (error) {
-      console.error("Error fetching current song:", error);
-      if (error.response && error.response.status === 401) {
-        // Spotify Access token expired, refresh it
-        const response = await axios.post(
-          "https://accounts.spotify.com/api/token",
-          new URLSearchParams({
-            grant_type: "refresh_token",
-            refresh_token: spotifyRefreshToken,
-          }),
+    botInterval = setInterval(async () => {
+      try {
+        console.log("Checking Spotify for currently playing song...");
+        const response = await axios.get(
+          "https://api.spotify.com/v1/me/player/currently-playing",
           {
             headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-              Authorization: `Basic ${Buffer.from(
-                `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
-              ).toString("base64")}`,
+              Authorization: `Bearer ${spotifyAccessToken}`,
             },
           }
         );
+        console.log("Spotify response:", response.status, response.data);
 
-        spotifyAccessToken = response.data.access_token;
-        saveTokens();
-      } else {
+        if (response.status === 200) {
+          const data = response.data;
+          const song = `${data.item.name} by ${data.item.artists
+            .map((artist) => artist.name)
+            .join(", ")}`;
+          if (song !== lastSong) {
+            console.log(`Sending message to Twitch chat: Now playing: ${song}`);
+            await twitchClient.say(twitchChannel, `Now playing: ${song}`);
+            lastSong = song;
+          }
+        }
+      } catch (error) {
         console.error("Error fetching current song:", error);
+        if (error.response && error.response.status === 401) {
+          // Spotify Access token expired, refresh it
+          const response = await axios.post(
+            "https://accounts.spotify.com/api/token",
+            new URLSearchParams({
+              grant_type: "refresh_token",
+              refresh_token: spotifyRefreshToken,
+            }),
+            {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: `Basic ${Buffer.from(
+                  `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+                ).toString("base64")}`,
+              },
+            }
+          );
+
+          spotifyAccessToken = response.data.access_token;
+          saveTokens();
+        } else {
+          console.error("Error fetching current song:", error);
+        }
       }
-    }
 
-    try {
-      // Test Twitch token
-      await axios.get(`https://api.twitch.tv/helix/users`, {
-        headers: {
-          "Client-ID": TWITCH_CLIENT_ID,
-          Authorization: `Bearer ${twitchAccessToken}`,
-        },
-      });
-    } catch (error) {
-      if (error.response && error.response.status === 401) {
-        // Twitch Access token expired, refresh it
-        const response = await axios.post(
-          `https://id.twitch.tv/oauth2/token`,
-          new URLSearchParams({
-            client_id: TWITCH_CLIENT_ID,
-            client_secret: TWITCH_CLIENT_SECRET,
-            grant_type: "refresh_token",
-            refresh_token: twitchRefreshToken,
-          })
-        );
+      try {
+        // Test Twitch token
+        await axios.get(`https://api.twitch.tv/helix/users`, {
+          headers: {
+            "Client-ID": TWITCH_CLIENT_ID,
+            Authorization: `Bearer ${twitchAccessToken}`,
+          },
+        });
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          // Twitch Access token expired, refresh it
+          const response = await axios.post(
+            `https://id.twitch.tv/oauth2/token`,
+            new URLSearchParams({
+              client_id: TWITCH_CLIENT_ID,
+              client_secret: TWITCH_CLIENT_SECRET,
+              grant_type: "refresh_token",
+              refresh_token: twitchRefreshToken,
+            })
+          );
 
-        twitchAccessToken = response.data.access_token;
-        twitchRefreshToken = response.data.refresh_token;
-        saveTokens();
-      } else {
-        console.error("Error testing Twitch token:", error);
+          twitchAccessToken = response.data.access_token;
+          twitchRefreshToken = response.data.refresh_token;
+          saveTokens();
+        } else {
+          console.error("Error testing Twitch token:", error);
+        }
       }
-    }
-  }, 10000);
+    }, 10000);
 
-  res.send("Bot is running!");
+    isBotRunning = true;
+    res.json({ message: "Bot is running!", isRunning: true });
+  } else {
+    if (twitchClient) {
+      twitchClient.disconnect();
+    }
+    clearInterval(botInterval);
+    isBotRunning = false;
+    res.json({ message: "Bot is stopped", isRunning: false });
+  }
+});
+
+app.get("/botStatus", (req, res) => {
+  res.json({
+    message: isBotRunning ? "Bot is running!" : "Bot is stopped",
+    isRunning: isBotRunning,
+  });
 });
 
 app.listen(port, () => {
